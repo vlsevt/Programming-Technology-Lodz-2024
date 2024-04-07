bd7c9b9c0ff12fe4b75f0bce7a240415f58864ba
904872 Sevtsenko Vladislav <vlsevt@taltech.ee>
2024-04-07 20:39:54 +0200

    Fixed a reference error

--- Warehouse_Logic_Layer/WarehouseLogic.cs
+++ Warehouse_Logic_Layer/WarehouseLogic.cs
@@ -1,19 +1,16 @@
-﻿namespace Warehouse_Logic_layer
+﻿public class WarehouseLogic
 {
-    public class WarehouseLogic
+    private WarehouseData _warehouse;
+    public WarehouseLogic(WarehouseData warehouse)
     {
-        private WarehouseData _warehouse;
-        public WarehouseLogic(WarehouseData warehouse)
-        {
-            _warehouse = warehouse;
-        }
-        public void RestockProduct(int productId, string productName, int quantity)
-        {
-            _warehouse.RecordIncomingShipment(productId, quantity);
-        }
-        public bool FulfillOrder(int productId, int quantity)
-        {
-            return _warehouse.RecordOutgoingShipment(productId, quantity);
-        }
+        _warehouse = warehouse;
+    }
+    public void RestockProduct(int productId, string productName, int quantity)
+    {
+        _warehouse.RecordIncomingShipment(productId, quantity);
+    }
+    public bool FulfillOrder(int productId, int quantity)
+    {
+        return _warehouse.RecordOutgoingShipment(productId, quantity);
     }
 }
