18d9affee40f78667a84c178b479ac163cd508c5
904872 Sevtsenko Vladislav <vlsevt@taltech.ee>
2024-04-07 20:22:45 +0200

    Implemented Warehouse_Logic_Layer and NUnit Tests.

---
+++ Warehouse_Logic_Layer/WarehouseLogic.cs
@@ -0,0 +1,19 @@
+﻿namespace Warehouse_Logic_layer
+{
+    public class WarehouseLogic
+    {
+        private WarehouseData _warehouse;
+        public WarehouseLogic(WarehouseData warehouse)
+        {
+            _warehouse = warehouse;
+        }
+        public void RestockProduct(int productId, string productName, int quantity)
+        {
+            _warehouse.RecordIncomingShipment(productId, quantity);
+        }
+        public bool FulfillOrder(int productId, int quantity)
+        {
+            return _warehouse.RecordOutgoingShipment(productId, quantity);
+        }
+    }
+}
