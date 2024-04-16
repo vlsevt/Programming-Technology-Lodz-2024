73438367f3962e21f1dcb8d108647ab94c446886
904872 Sevtsenko Vladislav <vlsevt@taltech.ee>
2024-04-16 18:50:07 +0200

    Person class added. Staff, customers and suppliers are instances of Person.

---
+++ Warehouse_Data_layer/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace WarehouseDataLayer
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} (ID: {Id})";
+        }
+
+    }
+}
