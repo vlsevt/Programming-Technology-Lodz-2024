eb3add6696a05ebc232be5ba311fdf22811eb981
Amaliya0 <125629086+Amaliya0@users.noreply.github.com>
2024-05-16 18:00:59 +0200

    -task2 DRAFT, MVVM: adding Model, Views and ViewModels

---
+++ WarehousePresentationLayer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WarehouseApp.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
