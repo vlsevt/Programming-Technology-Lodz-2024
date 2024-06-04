1cf64b2fe6420975486a934e6ba5e6d1ccffff13
Amaliya0 <125629086+Amaliya0@users.noreply.github.com>
2024-06-04 18:06:42 +0200

    -task2, rollback

---
+++ WarehousePresentation_M_VM/Model/Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehousePresentation_M_VM.Model
+{
+    internal class Model
+    {
+    }
+}
